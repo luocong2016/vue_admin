4afdfada2d942187537c8928f61d57f535cc3dd6
lutz <luocong2016@126.com>
2018-10-26 18:28:11 +0800

    .

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './plugins/element.js';
-import './permission';
+// import './permission';
 
 Vue.config.productionTip = false;
 
